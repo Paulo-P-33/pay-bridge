8c39c0b610811999e12bc0edfe3366bfbf9c16fa
Paulo-P-33 <luiz.lira029@gmail.com>
2024-06-04 11:05:14 -0300

    feat: basic struct to pay bridge api

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import express, {NextFunction} from 'express'
+import {IncomingMessage,Server, ServerResponse} from 'http'
+
+
+export const createMainServer = async (): Promise<Server<typeof IncomingMessage, typeof ServerResponse>> => {
+  const app = express()
+  app.use(
+    (
+      err: any,
+      _req: express.Request,
+      res: express.Response,
+      _next: NextFunction,
+    ) => {
+      console.error(err);
+      return res.status(err['status'] || 500).json({
+        message: err.message || 'Internal server error',
+        status: err['status'] || 500,
+      });
+    },
+  );
+
+  return app.listen(3333, () => console.log('Server is running!'))
+
+}
