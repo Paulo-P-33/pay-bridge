bb7eeacc8f8c5804287f6cf714821b1aa86bb44d
Paulo-P-33 <luiz.lira029@gmail.com>
2024-06-19 19:32:22 -0300

    refactor(validator): adjust function to validate schame nested

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -4,16 +4,21 @@ import { validate } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
 
 
-export function validateMiddleware(ClassType: any, partsRequest: string = 'body') {
+export function validateSchema(ClassType: any, partsRequest: string = 'body') {
   return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const input = plainToInstance(ClassType, req[`${partsRequest}` as 'body']);
     const errors = await validate(input);
-
     if (errors.length > 0) {
+
       const errorMessages = errors.map((err) => {
-        return { property: err.property, constraints: err.constraints };
+        return { 
+          property: err.property, 
+          constraints: err.children?.length as number > 0 
+            ? err.children?.map((error) => error.constraints) // Linha que checa validação aninhada
+            : err.constraints};
       });
-      res.status(400).json({ errors: errorMessages });
+      
+      res.status(400).json({ validationErrors: errorMessages });
       return;
     }
 
