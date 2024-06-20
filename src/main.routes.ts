93a4993b4e16977a063c20e21af5c606a4df877c
Paulo-P-33 <luiz.lira029@gmail.com>
2024-06-20 19:12:28 -0300

    feat: add route to create user of type customer

--- src/main.routes.ts
+++ src/main.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { authRoutes } from './modules/auth/auth.routes';
+import { userRoutes } from './modules/user/user.routes';
 const routes = Router();
 
 routes.use('/auth', authRoutes);
+routes.use('/user', userRoutes);
 
 routes.get('/', async (req, res) => {
   return res.status(200).json({message: 'OK'});
