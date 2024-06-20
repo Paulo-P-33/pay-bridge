d5f9f2b91da040d34af8f75be56bdca7605e284b
Paulo-P-33 <luiz.lira029@gmail.com>
2024-06-20 19:07:59 -0300

    refactor(.env.example): update with new environment variables

--- src/utils/env/config.ts
+++ src/utils/env/config.ts
@@ -1,6 +1,9 @@
 import env from 'env-var';
 
 export const config = {
+  databaseUrl: env.get('DATABASE_URL').required().asString(),
+  jwtSecret: env.get('JWT_SECRET').required().asString(), 
+  baseUrlApiPagarMe: env.get('BASE_URL_API_PAGARME').required().asString(),
   paymentAPiPublicKey: env.get('PAYMENT_API_PUBLIC_KEY').required().asString(),
-  paymentApiKey: env.get('PAYMENT_API_KEY').required().asString(), 
+  paymentApiKey: env.get('PAYMENT_API_KEY').required().asString(),
 };
